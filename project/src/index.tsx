ef4249dac48033411362169e1ae23f09700fbd41
Polina <polina-postylyakova@yandex.ru>
2023-03-24 20:22:45 +0300

    2.1. main page component, other page components are added

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const PromoFilm = {
+  nameFilm: 'The Grand Budapest Hotel',
+  genreFilm: 'Drama',
+  releaseYearFilm: 2014
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App nameFilm={PromoFilm.nameFilm} genreFilm={PromoFilm.genreFilm} releaseYearFilm={PromoFilm.releaseYearFilm} />
   </React.StrictMode>,
 );
